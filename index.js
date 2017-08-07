0b344af9020385382dea9e8e9831f778759803b8
Rahul Mittal <mittal_rahul@network.lilly.com>
2017-08-08 01:10:17 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
+﻿
 'use strict';
 const http = require('http');
 const host = 'api.worldweatheronline.com';
 const wwoApiKey = '[YOUR_API_KEY]';
 exports.weatherWebhook = (req, res) => {
   // Get the city and date from the request
-  let city = req.body.result.parameters['geo-city']; // city is a required param
+  var city = req.body.result.parameters['geo-city']; // city is a required param
   // Get the date for the weather forecast (if present)
-  let date = '';
+  var date = '';
   if (req.body.result.parameters['date']) {
     date = req.body.result.parameters['date'];
     console.log('Date: ' + date);
@@ -26,20 +26,20 @@ exports.weatherWebhook = (req, res) => {
 function callWeatherApi (city, date) {
   return new Promise((resolve, reject) => {
     // Create the path for the HTTP request to get the weather
-    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+    var path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
       '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
     console.log('API Request: ' + host + path);
     // Make the HTTP request to get the weather
     http.get({host: host, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
+      var body = ''; // var to store the response chunks
       res.on('data', (d) => { body += d; }); // store each response chunk
       res.on('end', () => {
         // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let forecast = response['data']['weather'][0];
-        let location = response['data']['request'][0];
-        let conditions = response['data']['current_condition'][0];
-        let currentConditions = conditions['weatherDesc'][0]['value'];
+        var response = JSON.parse(body);
+        var forecast = response['data']['weather'][0];
+        var location = response['data']['request'][0];
+        var conditions = response['data']['current_condition'][0];
+        var currentConditions = conditions['weatherDesc'][0]['value'];
         // Create response
         let output = `Current conditions in the ${location['type']} 
         ${location['query']} are ${currentConditions} with a projected high of

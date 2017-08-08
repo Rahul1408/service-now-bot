9b6099855f57a45bdf6b2c1ceba8452f16e6b422
Rahul Mittal <mittal_rahul@network.lilly.com>
2017-08-08 16:17:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,17 @@ var host = 'http://api.worldweatheronline.com';
 var wwoApiKey = 'e1affc06154840e8be8190125170708';
 app.get('/', function (req, res) {
     var options = {};
-    var city=req.body.result.parameters['geo-city']; // city is a required param
+    //var city=req.body.result.parameters['geo-city']; // city is a required param
+    var city='New York';
     // Get the date for the weather forecast (if present)
     var date = '';
-    if (req.body.result.parameters['date']) {
-        date = req.body.result.parameters['date'];
-        console.log('Date: ' + date);
-    }
+    //if (req.body.result.parameters['date']) {
+    //    date = req.body.result.parameters['date'];
+    //    console.log('Date: ' + date);
+    //}
     console.log('Date: ' + date);    
     var path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-     '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
+     '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + '2017-08-08';
     console.log('API Request: ' + host + path);
    
     options = {
@@ -43,4 +44,11 @@ res.setHeader('Content-Type', 'application/json');
 res.send(JSON.stringify({ 'speech': output, 'displayText': output,'source':'weather-sample' }));
 }
 });
-    })
+})
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+module.exports = app;
+
+
+
